9d6b271c830cb188014e2fe20c5c9fa669016a85
Johan <johanoriginalplus@gmail.com>
2023-04-14 10:51:28 -0500

    Encriptador working

--- src/Javascript/index.js
+++ src/Javascript/index.js
@@ -9,3 +9,32 @@ textField.addEventListener("input", (evt) => {
 		placeHolder.textContent = text
 	}
 })
+
+// ------------------------------------------------------------------------------------------------------------------------utils
+const logica = {a: "ai", e: "enter", i: "imes", o: "ober", u: "ufat"}
+
+// Esto ayuda a crear un regex que reemplaza el pattern por el replacer
+const regexFactory = (pattern, replacer) => {
+	const regexmaker = new RegExp(pattern, "g")
+
+	return (text) => {
+		return text.replace(regexmaker, replacer)
+	}
+}
+
+// ------------------------------------------------------------------------------------------------------------------------Logica encriptar
+
+let inicialEncriptador = {}
+const encriptador = Object.keys(logica).reduce((prev, x) => {
+	return {...prev, [x]: regexFactory(x, logica[x])}
+}, inicialEncriptador)
+
+const encriptar = (texto) => {
+	const arrayText = Array.from(texto)
+		.map((x) => (encriptador[x] ? encriptador[x](x) : x))
+		.join("")
+
+	return arrayText
+}
+
+// ------------------------------------------------------------------------------------------------------------------------
